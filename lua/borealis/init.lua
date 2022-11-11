19b3d3758b29bd7c1dc0a319f36956c70a9362e4
Gako358 <gako358@outlook.com>
2022-11-11 10:52:07 +0100

    minor refactoring

--- lua/borealis/init.lua
+++ lua/borealis/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 ---Change borealis option (vim.g.borealis_config.option)
----It can't be changed directly by modifing that field due to a Neovim lua bug with global variables (borealis_config is a global variable)
 ---@param opt string: option name
 ---@param value any: new value
 function M.set_option(opt, value)
