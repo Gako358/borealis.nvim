ed0544b054129b43433c2c7911d7c88d6b73c964
Gako358 <gako358@outlook.com>
2022-11-27 13:02:43 +0100

    updating indent-blankline input and tested

--- flake.nix
+++ flake.nix
@@ -46,8 +46,18 @@
           customRC = ''
             set termguicolors
             colorscheme borealis
+
             lua << EOF
             require("bufferline").setup{}
+            vim.opt.list = true
+
+            vim.opt.listchars:append "space:⋅"
+            vim.opt.listchars:append "eol:↴"
+            require("indent_blankline").setup {
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+            }
             EOF
           '';
         };
