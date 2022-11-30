8d7a1a6988557c3c22fb5fc104811324f5dc7c55
Gako358 <gako358@outlook.com>
2022-11-30 08:49:24 +0100

    lualine theme addet

---
+++ lua/lualine/themes/borealis.lua
@@ -0,0 +1,51 @@
+local c = require("borealis.colors")
+local colors = {
+  bg = c.bg0,
+  fg = c.fg,
+  grey = c.grey,
+  red = c.red,
+  blue = c.blue,
+  purple = c.purple,
+  green = c.green,
+  yellow = c.yellow,
+}
+
+local borealis = {
+  normal = {
+    a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.fg },
+    c = { bg = colors.bg, fg = colors.fg },
+  },
+  insert = {
+    a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.fg },
+    c = { bg = colors.bg, fg = colors.fg },
+  },
+  visual = {
+    a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.fg },
+    c = { bg = colors.bg, fg = colors.fg },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.fg },
+    c = { bg = colors.bg, fg = colors.fg },
+  },
+  command = {
+    a = { bg = colors.grey, fg = colors.bg, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.fg },
+    c = { bg = colors.bg, fg = colors.fg },
+  },
+  inactive = {
+    a = { bg = colors.bg, fg = colors.grey, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.grey },
+    c = { bg = colors.bg, fg = colors.grey },
+  },
+  terminal = {
+    a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+    b = { bg = colors.bg, fg = colors.fg },
+    c = { bg = colors.bg, fg = colors.fg },
+  },
+}
+
+return borealis;
