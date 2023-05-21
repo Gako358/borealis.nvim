c4298099f9c58b1c76cf776285ba27de07d52e7d
Gako358 <gako358@outlook.com>
2023-05-21 11:50:35 +0200

    update trouble bg

--- lua/borealis/plugins/trouble.lua
+++ lua/borealis/plugins/trouble.lua
@@ -2,9 +2,9 @@ local M = {}
 local c = require("borealis.colors").select_colors()
 
 M.highlights = {
-  TroubleText = { fg = c.green },
-  TroubleCount = { fg = c.purple, bg = c.bg2 },
-  TroubleNormal = { fg = c.fg, bg = c.bg2 },
+    TroubleText = {fg = c.fg2},
+    TroubleCount = {fg = c.purple, bg = c.bg1},
+    TroubleNormal = {fg = c.fg, bg = c.bg1}
 }
 
 return M
