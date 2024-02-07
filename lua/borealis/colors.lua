915b522af2e8499f0ca9774a21bbb564f57fc30f
Gako358 <gako358@outlook.com>
2024-02-07 07:40:49 +0100

    fix colors, update pallete

--- lua/borealis/colors.lua
+++ lua/borealis/colors.lua
@@ -1,37 +1,37 @@
 local M = {}
 local colors = {
     dark = {
-        fg = "#abb2bf",                 -- # Main foreground (Light Slate Gray)
-        fg1 = "#ebdbb2",                -- # Secondary foreground, variables
-        bg0 = "#272727",                -- # Main background (Dark Gray)
-        bg1 = "#262626",                -- # Menus and popups (Charcoal)
-        bg2 = "#3b3f4c",                -- # Statusline background (Midnight Blue)
-        bg3 = "#2b303b",                -- # nvim tree, terminal and telescope background (Dark Slate Gray)
+        fg = "#abb2bf", -- # Main foreground (Light Slate Gray)
+        fg1 = "#ebdbb2", -- # Secondary foreground, variables
+        bg0 = "#272727", -- # Main background (Dark Gray)
+        bg1 = "#262626", -- # Menus and popups (Charcoal)
+        bg2 = "#3b3f4c", -- # Statusline background (Midnight Blue)
+        bg3 = "#2b303b", -- # nvim tree, terminal and telescope background (Dark Slate Gray)
         black = "#252525",
-		blue = "#61afef",               -- # Functions and Titles
-        cyan = "#5fafb9",               -- # Builtin functions, fields
-        dark_blue = "#2C4F77",          -- # Search, Diff file, text, info, selections, tags
-        dark_cyan = "#1b6a73",          -- # Builtin functions, fields
-        dark_grey = "#5c6370",          -- # Comments
-        dark_green = "#2B3D28",         -- # Git diff add background, substitutions
-        dark_purple = "#854897",        -- # Special highlighting
-        dark_red = "#E45876",           -- # Special, Gid diff delete background, errors, exceptions
-		dark_yellow = "#FFA066",        -- # Constants, imports, booleans
+        blue = "#61afef", -- # Functions and Titles
+        cyan = "#5fafb9", -- # Builtin functions, fields
+        dark_blue = "#2C4F77", -- # Search, Diff file, text, info, selections, tags
+        dark_cyan = "#1b6a73", -- # Builtin functions, fields
+        dark_grey = "#5c6370", -- # Comments
+        dark_green = "#2B3D28", -- # Git diff add background, substitutions
+        dark_purple = "#854897", -- # Special highlighting
+        dark_red = "#E45876", -- # Special, Gid diff delete background, errors, exceptions
+        dark_yellow = "#FFA066", -- # Constants, imports, booleans
         grey = "#5c6370",
-		green = "#96E072",              -- # Strings, texts, diff changes
+        green = "#96E072", -- # Strings, texts, diff changes
         light_grey = "#808b98",
-        orange = "#FF8E3B",             -- # Warnings, diagnostic warnings
-        purple = "#c74ded",             -- # Statements and Keywords
-		red = "#e86671",                -- # Identifiers, LSP variables, Todos, Numbers
-        yellow = "#FFE66D"              -- # Operators, RegEx, questions
+        orange = "#FF8E3B", -- # Warnings, diagnostic warnings
+        purple = "#c74ded", -- # Statements and Keywords
+        red = "#e86671", -- # Identifiers, LSP variables, Todos, Numbers
+        yellow = "#FFE66D" -- # Operators, RegEx, questions
     },
     light = {
-        fg = "#383a42",                 -- Main foreground
-        fg1 = "#383a42",                -- Secondary foreground, variables
-        bg0 = "#fafafa",                -- Main background
-        bg1 = "#f0f0f0",                -- Menus and popups
-        bg2 = "#e6e6e6",                -- Statusline background
-        bg3 = "#dcdcdc",                -- nvim tree, terminal and telescope background
+        fg = "#383a42", -- Main foreground
+        fg1 = "#383a42", -- Secondary foreground, variables
+        bg0 = "#fafafa", -- Main background
+        bg1 = "#f0f0f0", -- Menus and popups
+        bg2 = "#e6e6e6", -- Statusline background
+        bg3 = "#dcdcdc", -- nvim tree, terminal and telescope background
         black = "#101012",
         blue = "#4078f2",
         cyan = "#0184bc",
@@ -49,7 +49,7 @@ local colors = {
         purple = "#a626a4",
         red = "#e45649",
         yellow = "#986801"
-    },
+    }
 }
 
 function M.terminal_color()
